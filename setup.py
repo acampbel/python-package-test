eb049919c523ee02d94d2513de797b7aafbbf893
Kaushal Sali <salikaushal@gmail.com>
2020-11-16 17:00:13 -0500

    test commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="python-package-test",
+    name="example-package-kaushalsali",
     version="0.0.1",
     author="Kaushal Sali",
     author_email="salikaushal@gmail.com",
