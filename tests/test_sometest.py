9b16794caca00f4e3881e603be0101ff91e849a0
Kaushal Sali <salikaushal@gmail.com>
2020-11-21 18:30:10 -0500

    tests updated. init.py updated.

---
+++ tests/test_sometest.py
@@ -0,0 +1,19 @@
+import unittest
+
+import mypackage
+
+
+class SomethingTest1(unittest.TestCase):
+    def test_is_running(self):
+        s = mypackage.do_something()
+        self.assertTrue(s == "Something")
+
+
+class SomethingTest2(unittest.TestCase):
+    def test_is_running(self):
+        s = mypackage.do_something()
+        self.assertFalse(s == "Otherthing")
+
+
+if __name__ == '__main__':
+    unittest.main()
