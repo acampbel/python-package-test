ac4edd88aa74abc6d66a402c484a2dd5d69e4301
Kaushal Sali <salikaushal@gmail.com>
2020-12-08 16:28:48 -0500

    Test updated

--- tests/test_sometest.py
+++ tests/test_sometest.py
@@ -1,19 +1,17 @@
 import unittest
 
-#import mypackage
+import mypackage
 
 
 class SomethingTest1(unittest.TestCase):
     def test_is_running(self):
- #       s = mypackage.do_something()
-        s = "Something"
+        s = mypackage.do_something()
         self.assertTrue(s == "Something")
 
 
 class SomethingTest2(unittest.TestCase):
     def test_is_running(self):
-        #s = mypackage.do_something()
-        s = "Something"
+        s = mypackage.do_something()
         self.assertFalse(s == "Otherthing")
 
 
